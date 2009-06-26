f7ea1304d7bf6b347671278bb0150703257e40f1
Casey Jones <jonescaseyb@gmail.com>
2009-06-25 21:53:34 -0500

    forgot my gpl stuff

--- src/id3.h
+++ src/id3.h
@@ -1,4 +1,24 @@
 /*
+ * File:   id3.h
+ * Author: Casey Jones
+ *
+ * This file is part of KaJamTag.
+ *
+ * KaJamTag is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KaJamTag is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with KaJamTag.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
  * Kajamtag ID3 tag reader
  */
 
